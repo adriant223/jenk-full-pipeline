d1a1d4ef5c9f50df11248ecbfa7baed7373521f6
adriant223 <adriantimis19@yahoo.com>
2024-03-16 18:52:10 +0200

    adding quality gates step

--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 import os
+import time
 from app import create_app
 
 app = create_app()
@@ -8,3 +9,4 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.run(host="0.0.0.0", port=port)
+
